f56ae41a96ee37e5ac559c58d1b77cbdbced4171
akalinin <am1kalinin@gmail.com>
2023-12-20 16:14:39 +0300

    start work on tile net

---
+++ frontend/app/src/components/TileNet.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="tileNet">
+        <img-tile
+            v-for="tile in tiles" :key="tile.id"
+            :tile="tile"
+        />
+    </div>
+</template>
+
+<script>
+import ImgTile from '@/components/ImgTile.vue'
+
+export default{
+    
+    data() {
+        return {
+            tiles:[{
+                j:0,
+                i:0,
+                k:0,
+                offsetX:0,
+                offsetY:0,
+                slideX: 0,
+                slideY: 0
+            },{
+                j:1,
+                i:0,
+                k:0,
+                offsetX:0,
+                offsetY:0,
+                slideX: 0,
+                slideY: 0
+            },{
+                j:0,
+                i:1,
+                k:0,
+                offsetX:0,
+                offsetY:0,
+                slideX: 0,
+                slideY: 0
+            },{
+                j:1,
+                i:1,
+                k:0,
+                offsetX:0,
+                offsetY:0,
+                slideX: 0,
+                slideY: 0
+            },]
+        };
+    },
+    components:{ImgTile}
+    
+}
+</script>
+
+<style scoped>
+</style>
--- frontend/app/src/components/ViewEdit.vue
+++ frontend/app/src/components/ViewEdit.vue
@@ -1,57 +1,16 @@
 <template>
     <div class="edit">
-        <img-tile
-            v-for="tile in tiles" :key="tile.id"
-            :tile="tile"
-        />
-        <div id="canvasBlock"></div>
+        <tile-net/>
         <svg id="svg" oncontextmenu="{return false;}"></svg>
     </div>
 </template>
 
 <script>
-import ImgTile from '@/components/ImgTile.vue'
+import TileNet from '@/components/TileNet.vue'
 
 export default{
-    
-    data() {
-        return {
-            tiles:[{
-                j:0,
-                i:0,
-                k:0,
-                offsetX:0,
-                offsetY:0,
-                slideX: 0,
-                slideY: 0
-            },{
-                j:1,
-                i:0,
-                k:0,
-                offsetX:0,
-                offsetY:0,
-                slideX: 0,
-                slideY: 0
-            },{
-                j:0,
-                i:1,
-                k:0,
-                offsetX:0,
-                offsetY:0,
-                slideX: 0,
-                slideY: 0
-            },{
-                j:1,
-                i:1,
-                k:0,
-                offsetX:0,
-                offsetY:0,
-                slideX: 0,
-                slideY: 0
-            },]
-        };
-    },
-    components:{ImgTile}
+
+    components:{TileNet}
     
 }
 </script>
