5016dceb6bd3d4d0243d89736db9b5cf2342df86
AMKalinin <am1kalinin@gmail.com>
2024-02-01 23:57:21 +0300

    add axios net

--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from 'vue';
+
+import axios from 'axios';
+
 import App from './App';
 
 import router from './router/router';
 
 
 const app = createApp(App);
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://api:8001/';
 
 app.use(router)
 
