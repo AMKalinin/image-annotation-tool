08040469b0ab75a77dc9a529601c8ab519f616b0
AMKalinin <am1kalinin@gmail.com>
2024-02-01 23:00:43 +0300

    add network for back and db

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
 from app.core.config import settings
 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+if not database_exists(engine.url):
+    create_database(engine.url)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
