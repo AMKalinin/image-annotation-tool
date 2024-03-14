8ec450b3dab330233cb41162abeba0ebe6e11117
akalinin <am1kalinin@gmail.com>
2024-03-14 16:17:30 +0300

    start work on project create

---
+++ frontend/app/src/components/CreateProjectDialog.vue
@@ -0,0 +1,195 @@
+<template>
+    <div class="modal-backdrop">
+      <div class="modal">
+        <header class="modal-header">
+          <slot name="header">
+            Create a new project
+          </slot>
+          <button
+            type="button"
+            class="btn-close"
+            @click="close"
+          >
+            x
+          </button>
+        </header>
+  
+        <section class="modal-body">
+                <p>
+                    Name
+                    <input 
+                        class="inputName" 
+                        type="text" 
+                        name="projectName" 
+                        :value=nameProject 
+                        @input="nameProject=$event.target.value"
+                    >
+                </p>
+
+                <p>
+                    Creator
+                    <input 
+                        class="inputName" 
+                        type="text" 
+                        name="creator" 
+                        :value=creator 
+                        @input="creator=$event.target.value"
+                    >
+                </p>
+
+                <p>
+                    Status
+                    <input 
+                        class="inputName" 
+                        type="text" 
+                        name="status" 
+                        :value=status 
+                        @input="status=$event.target.value"
+                    >
+                </p>
+
+                <p class="description">
+                    Description
+                    <textarea 
+                        class="inputDescription" 
+                        name="description" 
+                        :value=description 
+                        @input="description=$event.target.value"
+                    ></textarea>
+                </p>
+                
+                <p>
+                    Load file
+                    <input 
+                        class="inputFile" 
+                        type="file"
+                        multiple
+                        name="files" 
+                        @change="previewFiles($event)"
+                    >
+                </p>
+
+                <p><button @click="send">send</button></p>
+         </section>
+  
+        <footer class="modal-footer">
+          <button
+            type="button"
+            class="btn-green"
+            @click="close"
+          >
+            Close Modal
+          </button>
+        </footer>
+      </div>
+    </div>
+  </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'CreateProjectDialog',
+  data(){
+        return{
+            nameProject:'',
+            creator:'I am',
+            status:'OK',
+            description:'',
+            inputFiles:[],
+        }
+    },
+  methods: {
+    close(){
+        this.$emit('close');
+    },
+    previewFiles(event){
+        this.inputFiles = event.target.files;
+    },
+    send(){
+        const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+        let url = '/projects/create'
+        const formData = new FormData()
+        // for( var i = 0; i < this.inputFiles.length; i++ ){
+        //     let file = this.inputFiles[i];
+
+        //     formData.append('files[' + i + ']', file);
+        // }
+        formData.append('project_name', this.nameProject)
+        formData.append('status', this.status)
+        formData.append('creator', this.creator)
+        formData.append('description', this.description)
+        formData.append('files', this.inputFiles)
+        axios.post(url, formData, config)
+    }   
+  },
+};
+</script>
+
+<style>
+  .modal-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal {
+    min-width: 500px;
+    min-height: 500px;
+    background: #FFFFFF;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .modal-header,
+  .modal-footer {
+    padding: 15px;
+    display: flex;
+  }
+
+  .modal-header {
+    position: relative;
+    border-bottom: 1px solid #eeeeee;
+    color: #4AAE9B;
+    justify-content: space-between;
+  }
+
+  .modal-footer {
+    border-top: 1px solid #eeeeee;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
+
+  .modal-body {
+    position: relative;
+    padding: 20px 10px;
+  }
+
+  .btn-close {
+    position: absolute;
+    top: 0;
+    right: 0;
+    border: none;
+    font-size: 20px;
+    padding: 10px;
+    cursor: pointer;
+    font-weight: bold;
+    color: #4AAE9B;
+    background: transparent;
+  }
+
+  .btn-green {
+    color: white;
+    background: #4AAE9B;
+    border: 1px solid #4AAE9B;
+    border-radius: 2px;
+  }
+</style>
--- frontend/app/src/views/ProjectsTab.vue
+++ frontend/app/src/views/ProjectsTab.vue
@@ -1,19 +1,73 @@
 <template>
     <div>
+        <div class="create_project_tools">
+            <button id="createNew"
+                type="button"
+                class="btn"
+                @click="showModal">
+                    Create new project
+            </button>
+            <button id="createBased">Create project based on</button>
+        </div>
         <project-list/>
+        <create-project-dialog
+            v-show="isModalVisible"
+            @close="closeModal"
+        />
     </div>
 </template>
 
 <script>
 import ProjectList from '@/components/ProjectList.vue';
+import CreateProjectDialog from '@/components/CreateProjectDialog.vue';
 
 export default{
-    components:{ProjectList}
+    components:{ProjectList, CreateProjectDialog},
+    data() {
+      return {
+        isModalVisible: false,
+      };
+    },
+    methods: {
+      showModal() {
+        this.isModalVisible = true;
+      },
+      closeModal() {
+        this.isModalVisible = false;
+      }
+    }
 }
 
 </script>
 
 <style>
+.create_project_tools{
+    position: relative;
+    top: 20px;
+    left: 30px;
+    margin-bottom: 30px;
+}
+
+.create_project_tools button{
+    border-radius: 6px;
+    height: 35px;
+    font-size: 90%;
+}
+
+#createNew{
+    background: #2DA44E;
+    border: 1px solid #2A9147;
+    color: #FFFFFF;
+    margin-right: 45px;
+    min-width: 125px;
+}
+
+#createBased{
+    background: #F6F8FA;
+    border: 1px solid #D5D8DA;
+    color: #24292F;
+    min-width: 160px;
+}
 
 
 </style>
